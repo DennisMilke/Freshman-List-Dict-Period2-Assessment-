3f3eaddfc77e604722f14f4e85acc4dc373a69cf
DennisMilke <dmilke923@gmail.com>
2024-12-17 09:26:21 -0500

    Update Jikan.py

--- Jikan.py
+++ Jikan.py
@@ -1,3 +1,4 @@
+x=0
 import json
 
 # Open and load the JSON file
@@ -5,10 +6,25 @@ with open('Jikan.json', 'r',  encoding='utf-8-sig') as file:
     data = json.load(file)
 
 # Print the loaded data (optional)
-print(data)
-
-## print all anime that have a score greater than 9.0
-
-
+""" print(data) """
 
 ## Print all anime that stopped airing before 2015
+
+""" for anime in data['data']:
+    try:
+        if anime['aired']['prop']['to']['year'] < 2015:
+            print(data['data'][x]['title'])
+            x=x+1
+    except:
+        continue """
+    
+    
+
+##Print all anime whose synopsis has more than 80 characters
+
+""" for anime in data['data']:
+    if len(data['data'][x]['synopsis']) > 80:
+        print(data['data'][x]['title'])
+    x= x+1 """
+            
+
